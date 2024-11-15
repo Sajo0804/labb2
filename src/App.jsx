4e739e470dde83963911cc40f23995c99ce7382b
Sajo0804 <sandrajonsson86@live.se>
2024-11-15 17:10:05 +0100

    API-call

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import StarWarsPeople from '../components/StarWarsPeople'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <StarWarsPeople />
+
+    </>
+  )
+}
+
+export default App
